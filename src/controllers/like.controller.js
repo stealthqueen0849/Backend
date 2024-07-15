8f4128f03e70c33a418db5401198322875e22c53
stealthqueen0849 <radhika.ladha2022@vitstudent.ac.in>
2024-07-15 18:46:10 +0530

    "add get all liked videos"

--- src/controllers/like.controller.js
+++ src/controllers/like.controller.js
@@ -2,7 +2,7 @@ import { Like } from "../models/like.models.js";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiResponse from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
-import { isValidObjectId } from "mongoose";
+import mongoose, { isValidObjectId } from "mongoose";
 
 
 //toggle like on video
@@ -102,9 +102,105 @@ const toggleTweetLike = asyncHandler(async (req, res) => {
     )
 })
 
-//get all alreadyLiked videos
+//get all liked videos
 const getLikedVideos = asyncHandler(async (req, res) => {
+    const likedVideos = await Like.aggregate([
+        {
+            //this gives us all the like docs containing likedby==user
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(req.user?._id)
+            }
+        },
+        {
+            //in the above docs find the doc which has video 
+            $lookup:{
+                from: "videos",
+                localField: "video",
+                foreignField: "_id",
+                as: "likedVideo",
+                pipeline: [
+                    {
+                        // we get the whole video model data heree!!!
+                        // so we are immediately extracting the owner data
+                        $lookup:{
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        _id: 0,
+                                        username: 1,
+                                        "avatar.url":1,
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    //finding the subscribers
+                    {
+                        $lookup:{
+                            from: "subscriptions",
+                            localField: "_id",
+                            foreignField: "channel",
+                            as: "subscribers",
+                        }
+                    },
+                    {
+                        $addFields:{
+                            subscribersCount: {$size: "$subscribers"},
+                            isSubscribed: {
+                                $cond: {
+                                    if: {
+                                        $in: [req.user?._id, "$subscribers.subscriber"]
+                                    },
+                                    then: true,
+                                    else: false,
+                                }
+                            }
+                        }
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            owner: 1,
+                            title: 1,
+                            "videoFile.url": 1,
+                            views: 1,
+                            duration: 1,
+                            subscribersCount: 1,
+                            isSubscribed: 1
+                        }
+                    },
+                ]
+            }
+        },
+        //solely for frontend purpose
+        {
+            $addFields: {
+                likedVideo:{
+                    $first: "$likedVideo",
+                }
+            }
+        },
+        {
+            $project:{
+                _id:0,
+                likedBy: 1,
+                likedVideo: 1,
+            }
+        }
+    ])
+    if(!likedVideos) throw new ApiError(500, "Error while getting the liked videos")
+    
+    console.log(likedVideos)
 
+    res
+    .status(200)
+    .json(
+        new ApiResponse(likedVideos, 200, "Got all the liked videos ")
+    )
 })
 
 export {
